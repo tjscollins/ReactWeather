9860ed4a400a53ff2e54137d634ded56e3d65826
Tyler Collins <tjscollins@gmail.com>
2016-11-12 14:31:46 +1000

    Style About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,11 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About Page!</p>
+      <h1 className="text-center">About</h1>
+      <h4>Built Using React and Foundation</h4>
+      <p>This is a simple website built using the React JavaScript framework and styled with the Foundation CSS framework.</p>
+      <h4>Using OpenWeather Map API</h4>
+      <p>All the data this site displays is obtained through the use of the OpenWeather Map API in realtime.</p>
     </div>
   );
 };
